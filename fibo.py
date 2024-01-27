933a3da0bf6d7677e556967bf4a6cb0ac043cc43
Eduardo Marcelo Campusano <marcelocampusano@yahoo.com>
2024-01-27 11:52:45 -0300

    agrego fibo

---
+++ fibo.py
@@ -0,0 +1,14 @@
+# code to calculate fibonachi numbers ?
+
+def fibonacci(n):
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n):
+            a, b = b, a + b
+        return b
